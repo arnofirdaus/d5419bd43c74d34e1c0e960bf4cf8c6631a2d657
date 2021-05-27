2451471f00a15eb0f5281f9953533f770ad5747f
Arno Firdaus <ruaz.anfield@gmail.com>
2021-05-27 11:47:43 +0700

    add header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,49 @@
 import React from "react"
 import styled from "styled-components"
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 
 const Container = styled.div`
-  width: flex;
-	padding: ${props => props.theme.space.small};
-`;
+  display: flex;
+	padding: ${props => props.theme.spaces.small};
+`
+
+const ContainerText = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-left: ${props => props.theme.spaces.medium};
+`
+
+const ContainerAddress = styled.div`
+  display: flex;
+`
+
+const TextTitle = styled.span`
+	color: ${props => props.theme.colors.black};
+	font-size: ${props => props.theme.fontSizes.medium};
+	font-weight: bold;
+`
+
+const TextSubTitle = styled.span`
+	color: ${props => props.theme.colors.grey};
+	font-size: ${props => props.theme.fontSizes.verySmall};
+	text-transform: uppercase;
+`
 
 const Header = () => {
 	return(
 		<Container>
-			<ArrowBackIcon />
+			<ArrowBackIcon style={{color: "#424749", marginTop: "4px"}}/>
+			<ContainerText>
+				<TextSubTitle>Alamat Pengantaran</TextSubTitle>
+				<ContainerAddress>
+					<TextTitle>
+						Tokopedia Tower
+					</TextTitle>
+					<ExpandMore style={{color: "#f9423a"}} />
+				</ContainerAddress>
+			</ContainerText>
+
 		</Container>
 	)
 }
